a29b1fa129bc2f4c7a3cc37ac69ab5e3bb454d47
CambridgeXR <chris.macdonald@lucy.cam.ac.uk>
2025-08-13 14:45:45 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = '360-vr-player-cache-v3';
+const CACHE_NAME = '360-vr-player-cache-v3'; // bump version to clear old cache
 const FILES_TO_CACHE = [
   './',
   './index.html',
@@ -7,29 +7,33 @@ const FILES_TO_CACHE = [
   './icons/icon-512.png'
 ];
 
+// Install Service Worker and cache files
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(cache => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
       .then(() => self.skipWaiting())
   );
 });
 
+// Activate Service Worker and remove old caches
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
         cacheNames.map(cacheName => {
           if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
         })
-      )
-    )
+      );
+    }).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
+// Fetch handler - serve cached files when offline
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
