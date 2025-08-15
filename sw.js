bb02715129c54caae6f9d7b9a83165d7eb067680
CambridgeXR <chris.macdonald@lucy.cam.ac.uk>
2025-08-15 13:05:19 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const VERSION = '11'; // bump this for each deploy
+const VERSION = '12'; // bump this for each deploy
 const CACHE = `vr-offline-cache-v${VERSION}`;
 
 // Precache the app shell (versioned)
